36adc48affbae9f861625e13c3f5b602de08e57b
hjiayz <hjiayz@163.com>
2020-09-18 15:56:21 +0800

    0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,83 +1,84 @@
-use futures::channel::mpsc;
-use futures::channel::oneshot;
-#[allow(unused_imports)]
-use futures::stream::StreamExt;
+//! ## async once tool for lazy_static
+//! 
+//! # Examples
+//! ```rust
+//!    use lazy_static::lazy_static;
+//!    use tokio::runtime::Runtime;
+//!    use async_once::AsyncOnce;
+//!
+//!    lazy_static!{
+//!        static ref FOO : AsyncOnce<u32> = AsyncOnce::new(async{
+//!            1
+//!        });
+//!    }
+//!    let mut rt = Runtime::new().unwrap();
+//!    rt.block_on(async {
+//!        assert_eq!(FOO.get().await , &1)
+//!    })
+//! ```
+//! 
+//! ### run tests
+//! 
+//! ```
+//! cargo test
+//! wasm-pack test --node
+//! ```
+
+use futures::future::FutureExt;
 #[allow(unused_imports)]
 use std::future::Future;
-
+use std::pin::Pin;
+use std::sync::RwLock;
+use std::task::Context;
+use std::task::Poll;
 pub struct AsyncOnce<T> {
-    sender: mpsc::UnboundedSender<oneshot::Sender<T>>,
+    fut: RwLock<Result<T, Pin<Box<dyn Future<Output = T> + Sync + Send>>>>,
 }
 
-impl<T: 'static + Clone> AsyncOnce<T> {
-    #[cfg(all(
-        feature = "tokio",
-        not(feature = "async-std"),
-        not(feature = "wasm-bindgen-futures")
-    ))]
+impl<T> AsyncOnce<T> {
     pub fn new<F>(fut: F) -> AsyncOnce<T>
     where
-        F: Future<Output = T> + Send + 'static,
-        T: Send,
+        F: Future<Output = T> + 'static + Sync + Send,
     {
-        let (tx, mut rx) = mpsc::unbounded::<oneshot::Sender<T>>();
-        tokio::spawn(async move {
-            let val = fut.await;
-            while let Some(val_tx) = rx.next().await {
-                let _ = val_tx.send(val.clone());
-            }
-        });
-        AsyncOnce { sender: tx }
+        let fut: RwLock<Result<T, Pin<Box<dyn Future<Output = T> + Sync + Send>>>> =
+            RwLock::new(Err(Box::pin(fut)));
+        AsyncOnce { fut }
     }
-    #[cfg(all(
-        feature = "async-std",
-        not(feature = "tokio"),
-        not(feature = "wasm-bindgen-futures")
-    ))]
-    pub fn new<F>(fut: F) -> AsyncOnce<T>
-    where
-        F: Future<Output = T> + Send + 'static,
-        T: Send,
-    {
-        let (tx, mut rx) = mpsc::unbounded::<oneshot::Sender<T>>();
-        async_std::task::spawn(async move {
-            let val = fut.await;
-            while let Some(val_tx) = rx.next().await {
-                let _ = val_tx.send(val.clone());
-            }
-        });
-        AsyncOnce { sender: tx }
-    }
-    #[cfg(all(
-        feature = "wasm-bindgen-futures",
-        not(feature = "tokio"),
-        not(feature = "async-std")
-    ))]
-    pub fn new<F>(fut: F) -> AsyncOnce<T>
-    where
-        F: Future<Output = T> + 'static,
-    {
-        let (tx, mut rx) = mpsc::unbounded::<oneshot::Sender<T>>();
-        wasm_bindgen_futures::spawn_local(async move {
-            let val = fut.await;
-            while let Some(val_tx) = rx.next().await {
-                let _ = val_tx.send(val.clone());
-            }
-        });
-        AsyncOnce { sender: tx }
-    }
-    pub async fn get(&self) -> T {
-        let (tx, rx) = oneshot::channel();
-        self.sender.unbounded_send(tx).unwrap();
-        rx.await.unwrap()
+    pub async fn get(&'static self) -> &'static T {
+        self.await
     }
 }
 
-#[cfg(all(
-    feature = "tokio",
-    not(feature = "async-std"),
-    not(feature = "wasm-bindgen-futures")
-))]
+impl<T> Future for &'static AsyncOnce<T>
+where
+    T: 'static,
+{
+    type Output = &'static T;
+    fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<&'static T> {
+        loop {
+            if let Ok(val) = self.fut.try_read() {
+                if let Ok(val) = val.as_ref() {
+                    return Poll::Ready(unsafe { (val as *const T).as_ref().unwrap() });
+                }
+            }
+            if let Ok(mut fut) = self.fut.try_write() {
+                let mut result = None;
+                if let Err(fut) = fut.as_mut() {
+                    if let Poll::Ready(val) = fut.poll_unpin(cx) {
+                        result = Some(val);
+                    }
+                }
+                if result.is_some() {
+                    *fut = Ok(result.unwrap());
+                    continue;
+                }
+            }
+            break;
+        }
+        Poll::Pending
+    }
+}
+
 #[test]
 fn lazy_static_test_for_tokio() {
     use lazy_static::lazy_static;
@@ -86,14 +87,9 @@ fn lazy_static_test_for_tokio() {
         static ref FOO: AsyncOnce<u32> = AsyncOnce::new(async { 1 });
     }
     let mut rt = Runtime::new().unwrap();
-    rt.block_on(async { assert_eq!(FOO.get().await, 1) })
+    rt.block_on(async { assert_eq!(FOO.get().await, &1) })
 }
 
-#[cfg(all(
-    feature = "async-std",
-    not(feature = "tokio"),
-    not(feature = "wasm-bindgen-futures")
-))]
 #[test]
 fn lazy_static_test_for_async_std() {
     use async_std::task;
@@ -101,5 +97,5 @@ fn lazy_static_test_for_async_std() {
     lazy_static! {
         static ref FOO: AsyncOnce<u32> = AsyncOnce::new(async { 1 });
     }
-    task::block_on(async { assert_eq!(FOO.get().await, 1) })
+    task::block_on(async { assert_eq!(FOO.get().await, &1) })
 }
--- tests/wasm.rs
+++ tests/wasm.rs
@@ -4,11 +4,6 @@ extern crate wasm_bindgen_test;
 #[allow(unused_imports)]
 use wasm_bindgen_test::*;
 
-#[cfg(all(
-    feature = "wasm-bindgen-futures",
-    not(feature = "tokio"),
-    not(feature = "async-std")
-))]
 #[wasm_bindgen_test]
 async fn lazy_static_test_for_wasm() {
     use async_once::AsyncOnce;
@@ -16,5 +11,5 @@ async fn lazy_static_test_for_wasm() {
     lazy_static! {
         static ref FOO: AsyncOnce<u32> = AsyncOnce::new(async { 1 });
     }
-    assert_eq!(FOO.get().await, 1)
+    assert_eq!(FOO.get().await, &1)
 }
