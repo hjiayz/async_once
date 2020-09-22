e630b038c513bb93f0d418f98e3b4b8271ed352c
hjiayz <hjiayz@163.com>
2020-09-22 16:21:50 +0800

    cell

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! ## async once tool for lazy_static
-//! 
+//!
 //! # Examples
 //! ```rust
 //!    use lazy_static::lazy_static;
@@ -16,33 +16,38 @@
 //!        assert_eq!(FOO.get().await , &1)
 //!    })
 //! ```
-//! 
+//!
 //! ### run tests
-//! 
-//! ```
+//!
 //! cargo test
-//! wasm-pack test --node
-//! ```
+//! wasm-pack test --headless --chrome --firefox
+//!
 
 use futures::future::FutureExt;
+use std::cell::Cell;
 #[allow(unused_imports)]
 use std::future::Future;
 use std::pin::Pin;
 use std::sync::RwLock;
 use std::task::Context;
 use std::task::Poll;
-pub struct AsyncOnce<T> {
-    fut: RwLock<Result<T, Pin<Box<dyn Future<Output = T> + Sync + Send>>>>,
+
+pub struct AsyncOnce<T:'static> {
+    ptr: Cell<Option<&'static T>>,
+    fut: RwLock<Result<T, Pin<Box<dyn Future<Output = T>>>>>,
 }
+
+unsafe impl<T:'static> Sync for AsyncOnce<T> {}
 
 impl<T> AsyncOnce<T> {
     pub fn new<F>(fut: F) -> AsyncOnce<T>
     where
-        F: Future<Output = T> + 'static + Sync + Send,
+        F: Future<Output = T> + 'static,
     {
-        let fut: RwLock<Result<T, Pin<Box<dyn Future<Output = T> + Sync + Send>>>> =
-            RwLock::new(Err(Box::pin(fut)));
-        AsyncOnce { fut }
+        AsyncOnce {
+            ptr: Cell::new(None),
+            fut: RwLock::new(Err(Box::pin(fut))),
+        }
     }
     pub async fn get(&'static self) -> &'static T {
         self.await
@@ -50,12 +55,14 @@ impl<T> AsyncOnce<T> {
 }
 
 impl<T> Future for &'static AsyncOnce<T>
-where
-    T: 'static,
 {
     type Output = &'static T;
     fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<&'static T> {
         loop {
+            let ptr = self.ptr.get();
+            if let Some(ptr) = ptr {
+                return Poll::Ready(ptr);
+            }
             if let Ok(val) = self.fut.try_read() {
                 if let Ok(val) = val.as_ref() {
                     return Poll::Ready(unsafe { (val as *const T).as_ref().unwrap() });
@@ -70,6 +77,8 @@ where
                 }
                 if result.is_some() {
                     *fut = Ok(result.unwrap());
+                    let ptr = unsafe {(fut.as_ref().ok().unwrap() as *const T).as_ref().unwrap()};
+                    self.ptr.set(Some(ptr));
                     continue;
                 }
             }
@@ -82,20 +91,43 @@ where
 #[test]
 fn lazy_static_test_for_tokio() {
     use lazy_static::lazy_static;
+    use std::time::Duration;
     use tokio::runtime::Runtime;
+    use futures_timer::Delay;
     lazy_static! {
-        static ref FOO: AsyncOnce<u32> = AsyncOnce::new(async { 1 });
+        static ref FOO: AsyncOnce<u32> = AsyncOnce::new(async {
+            Delay::new(Duration::from_millis(100)).await;
+            1
+        });
     }
     let mut rt = Runtime::new().unwrap();
-    rt.block_on(async { assert_eq!(FOO.get().await, &1) })
+    rt.spawn(async { assert_eq!(FOO.get().await, &1) });
+    rt.spawn(async { assert_eq!(FOO.get().await, &1) });
+    rt.spawn(async { assert_eq!(FOO.get().await, &1) });
+    rt.block_on(async {
+        Delay::new(Duration::from_millis(200)).await;
+        assert_eq!(FOO.get().await, &1);
+    });
 }
 
+#[cfg(not(target_os = "unknown"))]
 #[test]
 fn lazy_static_test_for_async_std() {
     use async_std::task;
     use lazy_static::lazy_static;
+    use std::time::Duration;
+    use futures_timer::Delay;
     lazy_static! {
-        static ref FOO: AsyncOnce<u32> = AsyncOnce::new(async { 1 });
+        static ref FOO: AsyncOnce<u32> = AsyncOnce::new(async {
+            Delay::new(Duration::from_millis(100)).await;
+            1
+        });
     }
-    task::block_on(async { assert_eq!(FOO.get().await, &1) })
+    task::spawn(async { assert_eq!(FOO.get().await, &1) });
+    task::spawn(async { assert_eq!(FOO.get().await, &1) });
+    task::spawn(async { assert_eq!(FOO.get().await, &1) });
+    task::block_on(async {
+        Delay::new(Duration::from_millis(200)).await;
+        assert_eq!(FOO.get().await, &1);
+    });
 }
