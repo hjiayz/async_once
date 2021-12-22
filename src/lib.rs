f42421ce16d5aca2a277a139a0eb95cdb2fd1dde
hjiayz <hjiayz@163.com>
2021-12-22 11:19:41 +0800

    fix bug

--- src/lib.rs
+++ src/lib.rs
@@ -29,14 +29,21 @@ use std::cell::Cell;
 use std::future::Future;
 use std::pin::Pin;
 use std::ptr::null;
+use std::sync::Arc;
 use std::sync::Mutex;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
 use std::task::Context;
 use std::task::Poll;
+use std::task::Wake;
+use std::task::Waker;
 
-type Fut<T> = Mutex<Result<T, Pin<Box<dyn Future<Output = T>>>>>;
+
+type Fut<T> = Mutex<Result<T,Pin<Box<dyn Future<Output = T>>>>>;
 pub struct AsyncOnce<T: 'static> {
     ptr: Cell<*const T>,
     fut: Fut<T>,
+    waker: Arc<MyWaker>,
 }
 
 unsafe impl<T: 'static> Sync for AsyncOnce<T> {}
@@ -49,11 +56,33 @@ impl<T> AsyncOnce<T> {
         AsyncOnce {
             ptr: Cell::new(null()),
             fut: Mutex::new(Err(Box::pin(fut))),
+            waker: Arc::new(MyWaker{
+                is_ok:AtomicBool::new(false),
+                wakers:Mutex::new(Vec::new()),
+            }),
         }
     }
     #[inline(always)]
     pub fn get(&'static self) -> &'static Self {
         self
+    }
+}
+
+struct MyWaker{
+    is_ok:AtomicBool,
+    wakers:Mutex<Vec<Waker>>,
+}
+
+impl Wake for MyWaker {
+    fn wake_by_ref(self: &std::sync::Arc<Self>) {
+        self.clone().wake();
+    }
+
+    fn wake(self: std::sync::Arc<Self>) {
+        self.is_ok.store(true, Ordering::SeqCst);
+        let mut wakers = self.wakers.lock().unwrap();
+        wakers.pop().unwrap().wake();
+        drop(wakers);
     }
 }
 
@@ -64,24 +93,43 @@ impl<T> Future for &'static AsyncOnce<T> {
         if let Some(ptr) = unsafe { self.ptr.get().as_ref() } {
             return Poll::Ready(ptr);
         }
-        let mut val = self.fut.lock().unwrap();
-        if let Ok(read_value) = val.as_ref() {
-            return Poll::Ready(unsafe { (read_value as *const T).as_ref().unwrap() });
-        }
-        loop {
-            let mut result = None;
-            if let Err(val) = val.as_mut() {
-                if let Poll::Ready(val) = Pin::new(val).poll(cx) {
-                    result = Some(val);
+        let cxwaker=cx.waker().clone();
+        let mut wakers =  self.waker.wakers.lock().unwrap();
+        wakers.push(cxwaker);
+        let is_first_or_ready = wakers.len()==1 || self.waker.is_ok.load(Ordering::SeqCst);
+        drop(wakers);
+        let mut result = None;
+        let mut fut = self.fut.lock().unwrap();
+        match (is_first_or_ready,fut.as_mut()) {
+            (true,Err(fut)) => {
+                let waker = Waker::from(self.waker.clone());
+                let mut ctx = Context::from_waker(&waker);
+                match Pin::new(fut).poll(&mut ctx) {
+                    Poll::Ready(res) => {
+                        result=Some(res);
+                    },
+                    Poll::Pending => return Poll::Pending,
                 }
             }
-            if let Some(res) = result {
-                *val = Ok(res);
-                let ptr = val.as_ref().ok().unwrap() as *const T;
-                self.ptr.set(ptr);
-                return Poll::Ready(unsafe { &*ptr });
+            (true,Ok(res))=>{
+                return Poll::Ready(unsafe { (res as *const T).as_ref().unwrap() });
             }
+            _=>(),
         }
+        if let Some(res) = result {
+            *fut = Ok(res);
+            let ptr = fut.as_ref().ok().unwrap() as *const T;
+            self.ptr.set(ptr);
+            drop(fut);
+            let mut wakers = self.waker.wakers.lock().unwrap();
+            while let Some(waker) = wakers.pop() {
+                waker.wake();
+            }
+            drop(wakers);
+            return Poll::Ready(unsafe { &*ptr });
+        }
+        drop(fut);
+        Poll::Pending
     }
 }
 
@@ -94,6 +142,7 @@ fn lazy_static_test_for_tokio() {
     use tokio::runtime::Builder;
     lazy_static! {
         static ref FOO: AsyncOnce<u32> = AsyncOnce::new(async {
+            tokio::spawn(async { assert_eq!(FOO.get().await, &1) });
             Delay::new(Duration::from_millis(100)).await;
             1
         });
